1a451c0d2b1673694a6267a80b4554d4b2c5ca36
Selvarajks <selvarajks@gmail.com>
2019-04-07 21:38:27 +0800

    My first Commit

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,33 @@
+﻿using OdeToFood.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+       IEnumerable <Restaurant> GetAll();
+    }
+
+    public class InMemoreyRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+        public InMemoreyRestaurantData()
+        {
+            restaurants = new List<Restaurant>
+            {
+                new Restaurant{Id =1, Name ="Scotts Pizza",Location ="MaryLand" ,Cuisine = CuisineType.Italian },
+                new Restaurant{Id =2, Name ="Cinnamon club",Location ="London", Cuisine = CuisineType.Mexican},
+                new Restaurant{Id =3, Name ="PonnuSamy",Location ="California",Cuisine = CuisineType.Indian}
+
+             };
+
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
